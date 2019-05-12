7f625e5bbade572df3cbad9036cfe7a146c3ab3f
Arthur Gatin <agatin@umail.ucsb.edu>
2019-05-11 22:45:39 -0700

    simple commands done

--- gnol.js
+++ gnol.js
@@ -37,11 +37,11 @@ client.on('ready', () => {
     //bgdn.user.send("Hello, user:" + bgdn.user.username + ":" + bgdn.user.discriminator + ", nice to see you.")
     //console.log("message sent")
     
-    var date = new Date();
+    var date = new Date(Date.now());
     //console.log()
     //bot-experiments: 560673275232124929
     let bot_exp = client.channels.get("560673275232124929");
-    bot_exp.send('@here I am online!\nTime is ');
+    bot_exp.send('@here I am online!\nTime is ' + date);
 
     /* this code sends a text message in order I want
     let insta_avatar = new Discord.Attachment("https://i.ytimg.com/vi/nQ7XUCBcAQg/maxresdefault.jpg")
@@ -51,17 +51,26 @@ client.on('ready', () => {
     bot_exp.send("You shouldn't have shared my existence with your instagram, human", insta_avatar)
     */
 
+    const CMD_PREFIX = '!';
+
     client.on('message', (msg) =>{
         if(msg.author == client.user) return
         else if(msg.channel == bot_exp){
+
+            if(msg.content.indexOf(CMD_PREFIX) != 1){
+                var response = parseCommand(msg.content.substr(1).split(" "));
+                if(response != '') msg.reply(response);
+                return;
+            }
+
             mapNumMessages.set(msg.author.id, mapNumMessages.get(msg.author.id)+1)
             let numEnding = "th"
             if(mapNumMessages.get(msg.author.id)%10 == 1 && mapNumMessages.get(msg.author.id)!= 11) numEnding = "st"
             if(mapNumMessages.get(msg.author.id)%10 == 2 && mapNumMessages.get(msg.author.id)!= 12) numEnding = "nd"
             if(mapNumMessages.get(msg.author.id)%10 == 3 && mapNumMessages.get(msg.author.id)!= 13) numEnding = "rd"
             bot_exp.send(msg.author.toString() + " says: " + msg + '\n' + "It's their " + mapNumMessages.get(msg.author.id) + numEnding + " message!")
-            msg.react("ðŸ‘€")
-            if(msg.content.indexOf("nice") != -1) msg.react("ðŸ‘")
+            msg.react("👀")
+            if(msg.content.indexOf("nice") != -1) msg.react("👍")
         }
 
     })
@@ -69,4 +78,20 @@ client.on('ready', () => {
 
     console.log("finished")
 })
+
+function parseCommand(args){
+    switch(args[0]){
+        case "coinflip" :
+            var rand = Math.random();
+            if(rand > 0.5) return 'heads';
+            else return 'tails';
+        break;
+        case "rand" :
+            var ceiling = args.length > 1 ? parseInt(args[1]) : 10;
+            return Math.floor(Math.random()*ceiling + 1);
+        break; 
+        default:
+            return '';
+    }
+}
 
