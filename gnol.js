a421bc2f6ef00e033f03f1cae6bd9fe9a0144b60
ArthurG0 <agatin@umail.ucsb.edu>
2019-11-30 01:53:54 -0800

    now supports weather calls through OpenWeather API

--- gnol.js
+++ gnol.js
@@ -2,16 +2,18 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 require('dotenv/config');
 require('./.env');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 //console.log(process.env)
 const TOKEN = process.env.GNOL_BOT_TOKEN;
 let gnol_server;
 
 const helptext = "List of commands:\n- !coinflip - bot will respond with a message \"heads\" of \"tails\", tagging the user\n- !rand and !rand <argument> - bot will respond with a random number from 1 to the indicated argument (10 by default)" +
-"\n- !giverole @<user> <rolename> - if a role exists on a server, bot will grant a user such a role" +
-"\n- !revokeroles @<user> - bot will revoke all server roles of the user specified" +
-"\n- !howmany - bot will respond with the number of messages that exist in a channel since it was last deployed" +
-"\n- !help - bot will respons with the text describing different commands"
+    "\n- !giverole @<user> <rolename> - if a role exists on a server, bot will grant a user such a role" +
+    "\n- !revokeroles @<user> - bot will revoke all server roles of the user specified" +
+    "\n- !howmany - bot will respond with the number of messages that exist in a channel since it was last deployed" +
+    "\n- !help - bot will respons with the text describing different commands" + 
+    "\n- !weather <City> - bot will respond with the current weather for the specified city, if it can"
 
 
 
@@ -21,11 +23,16 @@ client.login(TOKEN);
 client.on('ready', () => {
     console.log("Connected as: " + client.user.tag)
 
-    console.log(helptext)
+
+    //this prints all available commands into console
+    //console.log(helptext)
+
+
+
     //client.user.setActivity("you from outside", {type: "WATCHING"})
 
     //prints name of all servers it's connected to
-    
+
     gnol_server = client.guilds.find(guild => guild.name === 'Gnol-Server')
 
     var mapNumMessages = new Map()
@@ -33,7 +40,7 @@ client.on('ready', () => {
         mapNumMessages.set(member.user.id, 0);
     })
 
-    //this lists all members in thÑƒ guild
+    //this lists all members in the guild
     // gnol_server.members.forEach((member) =>{
     //     console.log(member.user.id)
     //     console.log(member.user.username)
@@ -41,7 +48,9 @@ client.on('ready', () => {
     //     console.log('\n')
     // })
 
+    let me = gnol_server.members.find(member => member.user.username === 'ArthurG')
     let bgdn = gnol_server.members.find(member => member.user.discriminator === '8218')
+    let self = gnol_server.members.find(member => member.id === '534190085625741342')
     //console.log(bgdn.user.username)
     //bgdn.user.send("Hello, user:" + bgdn.user.username + ":" + bgdn.user.discriminator + ", nice to see you.")
     //console.log("message sent")
@@ -50,7 +59,11 @@ client.on('ready', () => {
     //console.log()
     //bot-experiments: 560673275232124929
     let bot_exp = client.channels.get("560673275232124929");
-    bot_exp.send('@here I am online!\nTime is ' + date);
+    
+    
+    
+    //bot greets
+    bot_exp.send(me.user.toString() + ' I am online!\nTime is ' + date.getHours() + ':' + date.getMinutes() + ':' + (date.getSeconds() > 9 ? date.getSeconds() : '0' + date.getSeconds()));
 
     /* this code sends a text message in order I want
     let insta_avatar = new Discord.Attachment("https://i.ytimg.com/vi/nQ7XUCBcAQg/maxresdefault.jpg")
@@ -60,8 +73,23 @@ client.on('ready', () => {
     bot_exp.send("You shouldn't have shared my existence with your instagram, human", insta_avatar)
     */
 
-    const CMD_PREFIX = '!';
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    const CMD_PREFIX = '!';
     client.on('message', (msg) => {
         if (msg.author == client.user) return
         else if (msg.channel == bot_exp) {
@@ -112,14 +140,14 @@ function parseCommand(args, message) {
             }
             else {
 
-                if (args[1].indexOf("@") === -1){
+                if (args[1].indexOf("@") === -1) {
                     message.channel.send('Usage: !giverole @<member> <roleName>');
                     return;
                 }
 
                 var authorMember = message.channel.guild.members.find(tmp => tmp.user.id === message.author.id)
                 var targetMember = message.channel.guild.members.find(tmp => tmp.user.id === message.mentions.users.array()[0].id)
-                if(!targetMember){
+                if (!targetMember) {
                     console.log('Undefined person');
                     return;
                 }
@@ -140,35 +168,84 @@ function parseCommand(args, message) {
             }
 
             break;
-            case "howmany":
-                console.log(message.channel.messages.size);
-                message.channel.send('Channel contains ' + message.channel.messages.size + ' messages')
+        case "howmany":
+            console.log(message.channel.messages.size);
+            message.channel.send('Channel contains ' + message.channel.messages.size + ' messages')
 
             break;
 
-            case "revokeroles":
-                if(args.length != 2 || !(args[1].startsWith("<@"))){
-                    console.log(args.length);
-                    console.log(args[1]);
-                    message.channel.send('Usage: !revokeroles @<member>');
+        case "revokeroles":
+            if (args.length != 2 || !(args[1].startsWith("<@"))) {
+                console.log(args.length);
+                console.log(args[1]);
+                message.channel.send('Usage: !revokeroles @<member>');
+                return;
+            }
+            else {
+                var targetMember = message.channel.guild.members.find(tmp => tmp.user.id === message.mentions.users.array()[0].id)
+                if (!targetMember) {
+                    console.log('Undefined person');
                     return;
                 }
-                else{
-                    var targetMember = message.channel.guild.members.find(tmp => tmp.user.id === message.mentions.users.array()[0].id)
-                    if(!targetMember){
-                        console.log('Undefined person');
-                        return;
-                    }
-                    targetMember.removeRoles(targetMember.roles);
-                    message.channel.send(targetMember.user.toString() + '\'s roles have been revoked')
-                }
+                targetMember.removeRoles(targetMember.roles);
+                message.channel.send(targetMember.user.toString() + '\'s roles have been revoked')
+            }
             break;
 
-            case "help":
-                if(args.length == 1){
-                    message.channel.send(helptext)
-                }
+        case "help":
+            if (args.length == 1) {
+                message.channel.send(helptext)
+            }
             break;
+
+        case "weather":
+            if (args.length < 2) {
+                console.log(args.length);
+                console.log(args[1]);
+                message.channel.send('Usage: !weather <City>');
+                return;
+            }
+            var cityArguments = "";
+            for(var i = 1; i < args.length; i++){
+                cityArguments += args[i];
+                if(i+1 < args.length) cityArguments += " ";
+            }
+            console.log(cityArguments);
+            
+            const httpRequest = new XMLHttpRequest();
+            var weather_url = "https://api.openweathermap.org/data/2.5/weather"
+            var full_url = weather_url + "?q=" + cityArguments + "&appid=" + process.env.OPEN_WEATHER_APIKEY;
+            console.log(full_url);
+            httpRequest.open("GET", full_url, true);
+            var weather_response;
+            httpRequest.onreadystatechange = function () {
+                //if the request has returned:
+                if (httpRequest.readyState === 4) {
+                    console.log('\n' + httpRequest.responseText);
+                    weather_response = JSON.parse(httpRequest.responseText);
+                    console.log('name of city: ' + weather_response.name);
+                    console.log('country' + weather_response.sys.country);
+                    console.log('weather, raw:cel ' + weather_response.main.temp + ':' + (weather_response.main.temp-273.15));
+                    console.log('weather description:' + weather_response.weather[0].description);
+                    console.log(message.author.toString());
+
+
+                    var messagetext = message.author.toString() + ", weather report for " + weather_response.name + "," + weather_response.sys.country + ":\n";
+                    messagetext +=  weather_response.weather[0].description + "\n";
+                    messagetext += "Temperature: " + (weather_response.main.temp-273.15) + "°C\n";
+                    messagetext += "Wind speed: " + weather_response.wind.speed + "m/s\n";
+
+                    message.channel.send(messagetext)
+
+
+
+
+                }
+            };
+            httpRequest.send();
+            console.log('sent!');
+            return;
+
 
         default:
             return;
